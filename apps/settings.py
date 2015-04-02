189ce6d59fb30818e50c95a5a634d2635dcb5a16
Eugene D <ei.dobrorodnov@gmail.com>
2015-04-02 11:17:27 +0300

    added db settings

--- apps/settings.py
+++ apps/settings.py
@@ -3,4 +3,4 @@
 class Settings(object):
 
     # Database settings
-    DB_PATCH = 'postgresql+psycopg2:///prom_chat'
+    DB_PATCH = 'postgres://aheyzrkyhmqpqj:46Wmld23KtuCo2s3Kdn7yPP6Vx@ec2-54-246-86-137.eu-west-1.compute.amazonaws.com:5432/daeuk22n8tjamt'
